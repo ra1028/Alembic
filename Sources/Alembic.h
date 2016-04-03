6385b07751e50cd33e8fc4b3edce8a9aecf0c165
Ryo Aoyama <r.fe51028.r@gmail.com>
2016-04-03 23:51:18 +0900

    tidy up

---
+++ Sources/Alembic.h
@@ -0,0 +1,19 @@
+//
+//  Alembic.h
+//  Alembic
+//
+//  Created by Ryo Aoyama on 4/3/16.
+//  Copyright © 2016 Ryo Aoyama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Alembic.
+FOUNDATION_EXPORT double AlembicVersionNumber;
+
+//! Project version string for Alembic.
+FOUNDATION_EXPORT const unsigned char AlembicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Alembic/PublicHeader.h>
+
+
