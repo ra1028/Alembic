c1ec3860e13cdd44621cbd5b3636938779e327e8
Ryo Aoyama <r.fe51028.r@gmail.com>
2016-09-28 03:57:21 +0900

    Remove test json files

--- Tests/AlembicTests/TestJSON.swift
+++ Tests/AlembicTests/TestJSON.swift
@@ -12,20 +12,9 @@ enum TestJSON {
     case distil
     case optional
     case transform
-    case serialize
-    
-    var fileName: String {
-        switch self {
-        case .distil: return "DistilTests"
-        case .optional: return "OptionalTests"
-        case .transform: return "TransformTests"
-        case .serialize: return "SerializeTests"
-        }
-    }
     
     var data: Data {
-        return Bundle(for: DistilTests.self).url(forResource: fileName, withExtension: "json")
-            .flatMap { try! Data.init(contentsOf: $0) }!
+        return try! JSONSerialization.data(withJSONObject: object, options: [])
     }
     
     var string: String {
@@ -33,6 +22,92 @@ enum TestJSON {
     }
     
     var object: Any {
-        return try! JSONSerialization.jsonObject(with: data, options: .allowFragments)
+        switch self {
+        case .distil: return distilTestJSONObject
+        case .optional: return optionalTestJSONObject
+        case .transform: return transformTestJSONObject
+        }
     }
 }
+
+private let distilTestJSONObject: [String: Any] = [
+    "string": "Alembic",
+    "int": 777,
+    "double": 77.7,
+    "float": 77.7 as Float,
+    "bool": true,
+    "array": [
+        "A", "B", "C"
+    ],
+    "dictionary": [
+        "A": 1, "B": 2, "C": 3
+    ],
+    "nested": [
+        "array": [
+            1, 2, 3, 4, 5
+        ]
+    ],
+    "int_string": "1",
+    "user": [
+        "id": 100,
+        "name": "ra1028",
+        "weight": 132.28,
+        "gender": "male",
+        "smoker": true,
+        "contact": [
+            "email": "r.fe51028.r@gmail.com",
+            "url": "https://github.com/ra1028"
+        ],
+        "friends": [
+            [
+                "id": 101,
+                "name": "ABCDE",
+                "weight": 120.00,
+                "gender": "female",
+                "smoker": false,
+                "contact": [
+                    "email": "abcde@example.com",
+                    "url": "https://example"
+                ],
+                "friends": [[String: Any]]()
+            ]
+        ]
+    ],
+    "numbers": [
+        "number": 1,
+        "int8": 2,
+        "uint8": 3,
+        "int16": 4,
+        "uint16": 5,
+        "int32": 6,
+        "uint32": 7,
+        "int64": 8,
+        "uint64": 9
+    ]
+]
+
+private let optionalTestJSONObject: [String: Any] = [
+    "string": "Alembic",
+    "int": 777,
+    "float": 77.7 as Float,
+    "bool": NSNull(),
+    "array": [
+        "A", "B", "C"
+    ],
+    "dictionary": [
+        "A": 1, "B": 2, "C": 3
+    ],
+    "nested": [String: Any](),
+    "user2": [
+        "id": 100,
+        "name": "ra1028",
+        "contact": [String: Any]()
+    ]
+]
+
+private let transformTestJSONObject: [String: Any] = [
+    "key": "value",
+    "null": NSNull(),
+    "array": [String](),
+    "nested": ["nested_key": "nested_value"]
+]
