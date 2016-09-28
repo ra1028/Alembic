fe24511411d8f9ae51dd83d1f05b5bc4bf7da7b9
Ryo Aoyama <r.fe51028.r@gmail.com>
2016-09-29 04:27:17 +0900

    Remove Sample

--- Sample/Package.swift
+++
@@ -1,8 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "Alembic-Sample",
-    dependencies: [
-        .Package(url: "https://github.com/ra1028/Alembic.git", majorVersion: 2),
-    ]
-)
--- Sample/README.md
+++
@@ -1,3 +0,0 @@
-# Alembic Sample
-
-### Learn how to use Alembic on [IBM Swift Sandbox](https://swiftlang.ng.bluemix.net/#/repl?gitPackage=https:%2F%2Fgithub.com%2Fra1028%2FAlembic%2FSample)
